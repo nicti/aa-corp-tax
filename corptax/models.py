d0922cfc04af461296bb52734230d68d719f4bae
David H <dhdavvie@gmail.com>
2021-06-06 01:48:45 +0100

    Implement tax owed logic for basic case

--- corptax/models.py
+++ corptax/models.py
@@ -10,6 +10,11 @@ class CorpTaxRate(models.Model):
     tax_rate = models.FloatField()
     date = models.DateField()
 
+    objects = models.Manager()
+
+    def __str__(self):
+        return f'{self.corp.corporation_name} had a tax rate of {self.tax_rate} on {self.date}'
+
     class Meta:
         unique_together = (("corp", "date"),)
 
@@ -17,10 +22,15 @@ class CorpTaxRate(models.Model):
 class CorpTaxOwed(models.Model):
     """The amount of tax a corp owes alliance based on income and their tax rate"""
     corp = models.ForeignKey(EveCorporationInfo, on_delete=models.DO_NOTHING)
-    month = models.IntegerField()
+    month = models.DateField()
     isk_owed = models.DecimalField(max_digits=15, decimal_places=2)  # TODO is this the best?
     paid = models.BooleanField(default=False)
     last_updated = models.DateTimeField(auto_now=True)
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return f'{self.corp.corporation_name} owes {self.isk_owed} for month of {self.month}. Paid? {self.paid}'
 
     class Meta:
         unique_together = (("corp", "month"),)
@@ -29,9 +39,16 @@ class CorpTaxOwed(models.Model):
 class CorpTaxSettings(models.Model):
     """The settings to represent which corp are taxed at which amount"""
     corp = models.ForeignKey(EveCorporationInfo, on_delete=models.DO_NOTHING, primary_key=True)
-    tax_rate = models.FloatField(null=True)
+    taxed_at = models.FloatField(null=True)
     taxed = models.BooleanField()
     last_updated = models.DateTimeField(auto_now=True)
+
+    objects = models.Manager()
+
+    def __str__(self):
+        if self.taxed:
+            return f'{self.corp.corporation_name} taxed at {self.taxed_at}'
+        return f'{self.corp.corporation_name} is not taxed'
 
 
 class Corptax(models.Model):
--- corptax/tasks.py
+++ corptax/tasks.py
@@ -1,11 +1,16 @@
-from datetime import datetime
+from calendar import monthrange
+from datetime import datetime, timedelta, date
+from functools import lru_cache
+from typing import Optional, Union
 
 import requests
+from django.db import models
 from celery import shared_task
 
 from allianceauth.services.hooks import get_extension_logger
 from allianceauth.corputils.models import EveCorporationInfo
 from corptax.models import CorpTaxRate, CorpTaxSettings, CorpTaxOwed
+from corptools.models import CorporationWalletJournalEntry
 
 logger = get_extension_logger(__name__)
 CORP_URL = 'https://esi.evetech.net/latest/corporations/{corp_id}/?datasource=tranquility'
@@ -44,4 +49,61 @@ def update_corps_in_corp_settings():
             settings.save()
             logger.info(f'Successfully created entry in CorpTaxSettings for {corp_name}')
 
-# TODO write task to periodically recalculate tax owed
+
+@lru_cache
+def _get_corp_tax_rate_for_day(corp: EveCorporationInfo, day: Union[datetime, date], strict: bool = False) -> float:
+    """
+    Returns the tax rate of the corp on this day. If `strict` is set to False and no tax rate is found for the given day
+    then the nearest tax rate known will be used. If no tax rate is found, None is returned
+    """
+    corp_tax_rate = CorpTaxRate.objects.filter(corp=corp, date=day)
+    if len(corp_tax_rate) == 1:
+        return corp_tax_rate[0].tax_rate
+    elif len(corp_tax_rate) > 1:
+        msg = f'Multiple entries found for {corp.corporation_name} on {day}!'
+        logger.error(msg)
+        raise SystemError(msg)
+    elif not strict:
+        # Look for tax rate of nearest day
+        closest_greater = CorpTaxRate.objects.filter(date__gt=day).order_by('date')
+        closest_less = CorpTaxRate.objects.filter(date__lt=day).order_by('-date')
+
+        closest_greater_diff = None
+        closest_less_diff = None
+        if len(closest_greater) > 0:
+            closest_greater_diff = closest_greater[0].date - day
+        if len(closest_less) > 0:
+            closest_less_diff = day - closest_less[0].date
+
+        if closest_greater_diff and closest_less_diff:
+            # Take the closest date, if tied then take the lesser date
+            return closest_greater[0].tax_rate if closest_greater_diff < closest_less_diff else closest_less[0].tax_rate
+        elif closest_less_diff is None:
+            return closest_greater[0].tax_rate
+        elif closest_greater_diff is None:
+            return closest_less[0].tax_rate
+        else:
+            logger.error(f'No tax rates found ever for {corp.corporation_name}')
+
+
+@shared_task
+def update_tax_owed(month: int, year: Optional[int] = None):
+    _get_corp_tax_rate_for_day.cache_clear()  # Clear the cache so that DB updates are handled
+    month = datetime(year=datetime.today().year if year is None else year, month=month, day=1)
+    end_of_month = month + timedelta(days=monthrange(month.year, month.month)[1])
+    for corp_settings in CorpTaxSettings.objects.filter(taxed=True, taxed_at__isnull=False):
+        logger.info(f'Processing tax owed for corp {corp_settings.corp.corporation_name}')
+        corp_id = corp_settings.corp.corporation_id
+        total_owed = 0
+        for wallet_entry in CorporationWalletJournalEntry.objects.filter(
+                tax_receiver_id=corp_id,
+                ref_type='bounty_prizes',
+                date__gte=month,
+                date__lt=end_of_month
+        ).order_by('date'):
+            tax_rate = _get_corp_tax_rate_for_day(corp_settings.corp, wallet_entry.date.date())
+            share_of_tax_owed_to_alliance = min(1.0, corp_settings.taxed_at/tax_rate)
+            total_owed += float(wallet_entry.amount) * share_of_tax_owed_to_alliance
+
+        corp_tax_owed, created = CorpTaxOwed.objects.update_or_create(corp=corp_settings.corp, month=month, defaults={'isk_owed': total_owed})
+        logger.info(corp_tax_owed)
